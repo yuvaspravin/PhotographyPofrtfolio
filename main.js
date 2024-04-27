3d441ebf0f110e3f1d6f2e559a4ec5476cb41718
sriram <sriram-it21>
2024-04-27 16:22:11 +0530

    name change

--- main.js
+++ main.js
@@ -83,18 +83,8 @@ ScrollReveal().reveal(".about__container img", {
   delay: 1500,
 });
 
-ScrollReveal().reveal(".service__container .section__header", {
-  ...scrollRevealOption,
-});
-ScrollReveal().reveal(".service__container .section__description", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".service__card", {
-  duration: 1000,
-  delay: 1000,
-  interval: 500,
-});
+
+
 
 
 
